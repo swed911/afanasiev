992608ba41bb23fb4d455bfa8378406f8f485906
Swed 911 <swed911@gmail.com>
2021-05-26 02:09:35 +0300

    test2

--- app/Http/Requests/BlogCategoryCreateRequest.php
+++ app/Http/Requests/BlogCategoryCreateRequest.php
@@ -7,9 +7,8 @@ use Illuminate\Foundation\Http\FormRequest;
 class BlogCategoryCreateRequest extends FormRequest
 {
     /**
-     * Determine if the user is authorized to make this request.
-     *
-     
+     * 
+
      */
     public function authorize()
     {
