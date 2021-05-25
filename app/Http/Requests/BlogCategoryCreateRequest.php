81bd6033a78d77813a0f67065542fddc34456cec
Swed 911 <swed911@gmail.com>
2021-05-26 02:07:18 +0300

    test

--- app/Http/Requests/BlogCategoryCreateRequest.php
+++ app/Http/Requests/BlogCategoryCreateRequest.php
@@ -9,7 +9,7 @@ class BlogCategoryCreateRequest extends FormRequest
     /**
      * Determine if the user is authorized to make this request.
      *
-     * @return bool
+     
      */
     public function authorize()
     {
